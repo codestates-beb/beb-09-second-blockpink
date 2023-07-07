f5e523eda91551db98beae2c6aa84e81734111f3
canu <cksdncjf@naver.com>
2023-07-07 16:39:40 +0900

    get userinfo api finished

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -4,24 +4,49 @@ const { Post, NFT, Users } = require("../models");
 
 module.exports = {
   getUserById: async (req, res) => {
+    const id = req.params.id;
     try {
       if (!req.headers.authorization) {
         return res.status(400).send("no authorization");
       } else {
         const token = req.headers.authorization.split(" ")[1];
-        const data = jwt.verify(token, process.env.ACESS_SECRET);
+        const data = jwt.verify(token, process.env.ACCESS_SECRET);
         const userId = data.userId;
 
-        const userInfo = Users.findOne({
+        const userInfo = await Users.findOne({
           where: {
             id: userId,
           },
         });
-
-        const userNft = NFT.findAll({
+        console.log(userInfo);
+        const userNft = await NFT.findAll({
           where: {
             n_userId: userId,
           },
+        });
+        console.log(userNft);
+        const nftData = userNft.map((nft) => {
+          return {
+            tokenURI: nft.ipfs_hash,
+            tokenId: nft.token_id,
+            txHash: nft.tx_hash,
+            description: nft.description,
+            createdAt: nft.createdAt,
+          };
+        });
+
+        const userPost = await Post.findAll({
+          where: {
+            p_userId: userId,
+          },
+        });
+        const postData = userPost.map((post) => {
+          return {
+            postId: post.id,
+            title: post.title,
+            content: post.content,
+            createdAt: post.createdAt,
+          };
         });
 
         return res.status(200).json({
@@ -29,19 +54,8 @@ module.exports = {
           address: userInfo.address,
           token_amount: userInfo.token_amount,
           eth_amount: userInfo.eth_amount,
-          nfts: {
-            tokenURI: "",
-            tokenId: 0,
-            txHash: "",
-            description: "",
-            createdAt: "",
-          },
-          posts: {
-            postId: 0,
-            title: "",
-            content: "",
-            createdAt: "",
-          },
+          nfts: nftData,
+          posts: postData,
         });
       }
     } catch (e) {
