21b016937ac38d53bee0a7844593825cea2d103c
23123121312312312323 <hot00has@naver.com>
2023-07-10 11:50:38 +0900

    feature/Spinner

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
+import Spinner from "./components/Spinner"; 
 
 import MainPage from "./pages/MainPage";
 import MyPage from "./pages/MyPage";
@@ -33,6 +34,9 @@ export default function App() {
     posts: [],
   });
 
+  const [loading, setLoading] = useState(true); // 로딩 상태 추가
+  const [pageLoading, setPageLoading] = useState(true); // 각 페이지 접속에 대한 로딩 상태 추가
+
   const value = useMemo(
     () => ({
       user,
@@ -40,12 +44,36 @@ export default function App() {
     }),
     [user, setUser]
   );
+  
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 700)); // 0.7초 딜레이 추가
+
+        setLoading(false); // 로딩 상태 업데이트
+      } catch (error) {
+        console.error(error.response); // 오류 응답 로깅
+        // 오류 처리 로직 추가
+        // 예: 오류 메시지를 사용자에게 표시하거나, 기본 데이터로 대체하거나, 다른 오류 처리 작업 수행 등
+        setLoading(false); // 로딩 상태를 false로 설정하여 로딩 컴포넌트를 제거하고, 오류 처리 작업을 수행할 수 있도록 함
+      }
+    }
+
+    fetchData();
+  }, []); // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때만 실행되도록 설정
+
+  useEffect(() => {
+    setPageLoading(loading); // 페이지 로딩 상태 업데이트
+  }, [loading]);
 
   return (
     <UserContext.Provider value={value}>
       <Router>
         <Header />
         <Sidebar />
+        {pageLoading ? (
+          <Spinner /> // 로딩 중이면 로딩 컴포넌트를 반환
+        ) : (
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/mypage" element={<MyPage />} />
@@ -58,6 +86,7 @@ export default function App() {
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
+        )}
         <Footer />
       </Router>
     </UserContext.Provider>
